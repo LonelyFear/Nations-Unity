6ede47de803747b351a25782055a763c5817d8f4
LonelyFear <chriskynwell@gmail.com>
2024-10-18 23:09:58 -0400

    More camera improvements

--- Assets/Scripts/UI/CameraController.cs
+++ Assets/Scripts/UI/CameraController.cs
@@ -24,17 +24,26 @@ public class CameraController : MonoBehaviour
 
     // Velocity
     Vector2 velocity = new Vector2();
+
+    Vector2Int worldSize;
     float zoomVel;
 
     // Camera
     Camera cam;
     void Start(){
         cam = GetComponent<Camera>();
+        MoveToCenter();
+    }
+
+    void MoveToCenter(){
+        worldSize = new Vector2Int(FindAnyObjectByType<GenerateWorld>().worldSize.x, FindAnyObjectByType<GenerateWorld>().worldSize.y);
+        transform.position = new Vector3(worldSize.x/2, worldSize.y/2, -10);
     }
     void Update()
     {
         moveCamera();
         transform.position += new Vector3(velocity.x, velocity.y) * Time.deltaTime;
+        clampPosition();
         bool overUI = EventSystem.current.IsPointerOverGameObject();
 
         if (!overUI){
@@ -42,12 +51,22 @@ public class CameraController : MonoBehaviour
         }  
     }
 
+    void clampPosition(){
+        float tx = transform.position.x;
+        float ty = transform.position.y;
+
+        tx = Mathf.Clamp(tx, 0, worldSize.x);
+        ty = Mathf.Clamp(ty, 0, worldSize.y);
+
+        transform.position = new Vector3(tx, ty, -10);
+    }
+
     void moveCamera(){
         InputAction move = InputSystem.actions.FindAction("Move");
         velocity += move.ReadValue<Vector2>() * (cameraSpeed * (cameraSize / maxZoom));
 
         // Deceleration
-        velocity = Vector2.Lerp(velocity, Vector2.zero, moveDamping * Time.deltaTime);
+        velocity = Vector2.SmoothDamp(velocity, Vector2.zero, ref velocity, moveDamping * Time.deltaTime);
 
         clampVelocity();
     }
@@ -61,11 +80,11 @@ public class CameraController : MonoBehaviour
     void zoomCamera(){
         InputAction zoom = InputSystem.actions.FindAction("Zoom");
         // Adds zoom speed to vel
-        zoomVel += zoomSpeed * Mathf.Round(zoom.ReadValue<float>());
+        zoomVel += zoomSpeed * Math.Sign(zoom.ReadValue<float>());
         // Clamps speed
         zoomVel = Mathf.Clamp(zoomVel, -zoomSpeed, zoomSpeed);
         // Deceleration
-        zoomVel = Mathf.Lerp(zoomVel, zoomVel * Mathf.Round(zoom.ReadValue<float>()), zoomDamping * Time.deltaTime);
+        zoomVel = Mathf.SmoothDamp(zoomVel, zoomVel * Math.Sign(zoom.ReadValue<float>()), ref zoomVel, zoomDamping * Time.deltaTime);
         // changes camera size by vel
         cameraSize += zoomVel;
 
